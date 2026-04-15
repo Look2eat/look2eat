0672501deb245b979ecfc7a33abc6bda16c37b5c
mansingh-04 <manwork.tr@gmail.com>
2026-04-16 03:23:45 +0530

    refactor: remove unused and deprecated utility files

--- backend/src/generated/prisma/enums.ts
+++
@@ -1,7 +0,0 @@
-export const Role = {
-  OWNER: 'OWNER',
-  OUTLET_MANAGER: 'OUTLET_MANAGER',
-  CASHIER: 'CASHIER'
-} as const
-
-export type Role = (typeof Role)[keyof typeof Role]
--- backend/src/generated/prisma/models.ts
+++
@@ -1,4 +0,0 @@
-export type * from './models/Brand'
-export type * from './models/AdminUser'
-export type * from './models/Outlet'
-export type * from './commonInputTypes'
