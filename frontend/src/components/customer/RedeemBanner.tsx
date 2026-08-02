289e1253713b297d0f9460e7a3a0b14dec376948
mansingh-04 <manwork.tr@gmail.com>
2026-08-02 13:59:16 +0530

    style: adjust RedeemBanner layout padding and typography sizing

--- frontend/src/components/customer/RedeemBanner.tsx
+++ frontend/src/components/customer/RedeemBanner.tsx
@@ -6,15 +6,15 @@ export default function RedeemBanner({ points }: RedeemBannerProps) {
   if (points <= 0) return null;
 
   return (
-    <div className="mx-4 mt-6 rounded-3xl  py-8 px-6 text-center">
+    <div className="mx-4 mt-4 rounded-3xl  py-12 px-6 text-center">
       <p className="text-xl font-semibold text-gray-800">
         Get Any Item Worth Up To
       </p>
 
-      <h2 className="mt-3 text-5xl font-extrabold leading-none">
-        ₹{points}{" "}
-        <span className="text-pink-500">
-          for Free
+      <h2 className="mt-3 text-3xl font-extrabold leading-none text-black">
+        ₹{points}{" "} for
+        <span className="text-pink-500 ">
+          Free
         </span>
       </h2>
     </div>
