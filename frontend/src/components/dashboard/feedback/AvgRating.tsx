86f5f8923cbc06440466bee5832bc63cbb4e7ee0
Avneet Singh <avneet0419@gmail.com>
2026-08-05 09:55:00 +0530

    feat: update avg rating component

--- frontend/src/components/dashboard/feedback/AvgRating.tsx
+++ frontend/src/components/dashboard/feedback/AvgRating.tsx
@@ -15,7 +15,7 @@ export default function AvgRating() {
     const { data, loading } = useFeedback();
 
     return (
-        <div className="flex bg-background">
+        <div className="flex w-full lg:w-auto bg-background">
             <TravelRouteCard
                 titleRating="Average Rating"
                 titleReviewPushed="Reviews pushed to Google"
@@ -30,7 +30,7 @@ export default function AvgRating() {
                         : data ? String(data.googleReviews) : "0"
                 }
                 imageUrl="https://plus.unsplash.com/premium_photo-1682310144714-cb77b1e6d64a?q=80&w=2712&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                className="font-poppins"
+                className="font-poppins w-full max-w-xl"
             />
         </div>
     );
