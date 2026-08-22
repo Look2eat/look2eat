9ec0cdaaee41c3b1deae6b26d24ee1ffcd4b7f6a
Avneet Singh <avneet0419@gmail.com>
2026-08-22 12:35:00 +0530

    refactor: update feedback card block

--- frontend/src/components/shadcn-space/blocks/dashboard-shell-01/FeedbackCard.tsx
+++ frontend/src/components/shadcn-space/blocks/dashboard-shell-01/FeedbackCard.tsx
@@ -5,8 +5,8 @@ import { FeedbackProvider } from '../../../../lib/auth/FeedbackContext'
 function FeedbackCard() {
     return (
         <FeedbackProvider>
-            <div className="w-full overflow-hidden relative rounded-2xl p-10 text-xl md:text-4xl font-bold text-foreground bg-white dark:bg-[#121214] font-poppins">
-                <FeedbackStats classname='bg-background dark:bg-[#1E1F27] h-39' />
+            <div className="w-full overflow-hidden relative rounded-2xl p-4 sm:p-6 md:p-10 text-xl md:text-4xl font-bold text-foreground bg-white dark:bg-[#121214] font-poppins">
+                <FeedbackStats classname='bg-background dark:bg-[#1E1F27] h-auto lg:h-39' />
             </div>
         </FeedbackProvider>
     )
